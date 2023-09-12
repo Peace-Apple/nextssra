4d9301644c12c30823429126220a0652a7dee547
peaceapple <astonreba@gmail.com>
2023-09-12 14:15:17 +0300

    Extract menu component

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,6 @@ class Index extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-                    <div className="navbar">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Speakers</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Sessions</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-
                 <div className="container">
                     <div className="row">
                         <div className="col margintopbottom">
--- src/Layout.js
+++ src/Layout.js
@@ -3,6 +3,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/site.module.css';
 import { Header } from "./Header";
+import { Menu } from "./Menu";
 
 export class Layout extends React.Component {
     render() {
@@ -11,6 +12,7 @@ export class Layout extends React.Component {
         return (
             <React.Fragment>
                 <Header />
+                <Menu />
                 {children}
             </React.Fragment>
         );
