0892553b68391a6b69780acac83c7211a2785019
peaceapple <astonreba@gmail.com>
2023-09-13 10:52:25 +0300

    Configure default environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withCSS = require("@zeit/next-css");
-
 require("dotenv").config();
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
@@ -7,6 +6,18 @@ const Dotenv = require("dotenv-webpack");
 /* Without CSS Modules, with PostCSS */
 module.exports = withCSS(
     {
+        serverRuntimeConfig: {
+            // Will only be available on the server side
+        },
+        publicRuntimeConfig: {
+            // Will be available on both server and client
+            RESTURL_SPEAKERS_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/speakers/ps",
+            RESTURL_SPEAKER_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/speaker",
+            RESTURL_SESSIONS_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/sessions"
+        },
         webpack(config, options) {
             config.plugins = config.plugins || [];
             config.plugins = [
