1a0b74e76fc30932085e4e2bd561f0aedc4bb5f8
peaceapple <astonreba@gmail.com>
2023-09-13 10:48:45 +0300

    Modify get speakers url

--- pages/speakers.js
+++ pages/speakers.js
@@ -2,12 +2,22 @@ import React from "react";
 import axios from 'axios';
 import SpeakerCard from "../src/SpeakerCard";
 
+import getConfig from 'next/config';
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig();
 
 class Speakers extends React.Component {
 
-    static async getInitialProps () {
-        var promise = axios.get('http://localhost:4000/speakers').
-        then(response => {
+    static GetSpeakersUrl() {
+        if (process.env.NODE_ENV === "production") {
+            return process.env.RESTURL_SPEAKERS_PROD
+                || publicRuntimeConfig.RESTURL_SPEAKERS_PROD;
+        } else {
+            return process.env.RESTURL_SPEAKERS_DEV;
+        }
+    }
+
+    static async getInitialProps() {
+        var promise = axios.get(Speakers.GetSpeakersUrl()).then(response => {
             return {
                 hasErrored: false,
                 speakerData: response.data
@@ -21,6 +31,7 @@ class Speakers extends React.Component {
             });
         return promise;
     }
+
 
     constructor(props) {
         super(props);
@@ -46,11 +57,11 @@ class Speakers extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="card-deck">
-                        {this.state.speakerData.map((speaker) =>
+                        {this.state.speakerData?(this.state.speakerData.map((speaker) =>
                             <div className="card col-4 cardmin margintopbottom20" key={speaker.id}>
                                 <SpeakerCard speaker={speaker}/>
                             </div>
-                        )}
+                        )):''}
                     </div>
                 </div>
             </div>
